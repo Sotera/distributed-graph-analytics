bc00df105c87c852526538ec464fa94913910a58
Jonathan Ostinowsky <jonathan.ostinowsky@soteradefense.com>
2014-05-23 08:55:06 -0700

    Added Proper logging instead of System.out

--- dga-giraph/src/main/java/com/soteradefense/dga/pagerank/PageRankMasterCompute.java
+++ dga-giraph/src/main/java/com/soteradefense/dga/pagerank/PageRankMasterCompute.java
@@ -3,7 +3,8 @@ package com.soteradefense.dga.pagerank;
 import org.apache.giraph.aggregators.DoubleMaxAggregator;
 import org.apache.giraph.master.DefaultMasterCompute;
 import org.apache.hadoop.io.DoubleWritable;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PageRankMasterCompute extends DefaultMasterCompute {
 
@@ -19,11 +20,12 @@ public class PageRankMasterCompute extends DefaultMasterCompute {
     public void compute() {
         long step = this.getSuperstep();
         if (step > 1) {
+            Logger logger = LoggerFactory.getLogger(PageRankMasterCompute.class);
             double maxDelta = ((DoubleWritable) this.getAggregatedValue(PageRankCompute.MAX_EPSILON)).get();
-            System.out.println("step: " + step + " max delta: " + maxDelta);
+            logger.info("step: " + step + " max delta: " + maxDelta);
             if (maxDelta < EPSILON) {
                 this.haltComputation();
-                System.out.println(maxDelta + " < " + EPSILON + " halting computation");
+                logger.info(maxDelta + " < " + EPSILON + " halting computation");
             }
         }
 
