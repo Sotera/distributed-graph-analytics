4e7808217f4f38fdae8361d48973183626667dbe
Jonathan Ostinowsky <jonathan.ostinowsky@soteradefense.com>
2014-06-03 16:15:23 -0700

    Page Rank Fixes

--- dga-giraph/src/main/java/com/soteradefense/dga/pr/PageRankComputation.java
+++ dga-giraph/src/main/java/com/soteradefense/dga/pr/PageRankComputation.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.io.Text;
 
 import java.io.IOException;
 
-public class PageRankComputation extends BasicComputation<Text, DoubleWritable, Text, DoubleWritable> {
+public class PageRankComputation extends BasicComputation<Text, Text, Text, DoubleWritable> {
 
     public static final String MAX_EPSILON = "com.soteradefense.dga.max.epsilon";
     public static final String DAMPING_FACTOR = "damping.factor";
@@ -15,16 +15,15 @@ public class PageRankComputation extends BasicComputation<Text, DoubleWritable, 
 
 
     @Override
-    public void compute(Vertex<Text, DoubleWritable, Text> vertex, Iterable<DoubleWritable> messages) throws IOException {
+    public void compute(Vertex<Text, Text, Text> vertex, Iterable<DoubleWritable> messages) throws IOException {
 
         float dampingFactor = this.getConf().getFloat(DAMPING_FACTOR, DAMPING_FACTOR_DEFAULT_VALUE);
 
         long step = getSuperstep();
 
-        // count the total number of nodes.
         if (step == 0) {
             //set initial value
-            vertex.setValue(new DoubleWritable(1.0 / getTotalNumVertices()));
+            vertex.setValue(getFormattedVertexValue(1.0 / getTotalNumVertices()));
         } else { // go until no one votes to continue
 
             double rank = 0;
@@ -32,16 +31,25 @@ public class PageRankComputation extends BasicComputation<Text, DoubleWritable, 
                 rank += partial.get();
             }
             rank = ((1 - dampingFactor) / getTotalNumVertices()) + (dampingFactor * rank);
-            double delta = Math.abs(rank - vertex.getValue().get()) / vertex.getValue().get();
+            double vertexValue = getFormattedVertexValue(vertex.getValue()).get();
+            double delta = Math.abs(rank - vertexValue / vertexValue);
             aggregate(MAX_EPSILON, new DoubleWritable(delta));
-            vertex.setValue(new DoubleWritable(rank));
+            vertex.setValue(getFormattedVertexValue(rank));
         }
         distributeRank(vertex);
     }
 
-    private void distributeRank(Vertex<Text, DoubleWritable, Text> vertex) {
-        double rank = vertex.getValue().get() / vertex.getNumEdges();
+    private void distributeRank(Vertex<Text, Text, Text> vertex) {
+        double rank = getFormattedVertexValue(vertex.getValue()).get() / vertex.getNumEdges();
         sendMessageToAllEdges(vertex, new DoubleWritable(rank));
     }
 
+
+    private Text getFormattedVertexValue(double val) {
+        return new Text(String.valueOf(val));
+    }
+
+    private DoubleWritable getFormattedVertexValue(Text val) {
+        return new DoubleWritable(Double.parseDouble(val.toString()));
+    }
 }
