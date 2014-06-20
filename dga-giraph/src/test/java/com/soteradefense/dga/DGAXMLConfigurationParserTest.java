d77304a72494ab797f0dad3e11747df68ab01876
Jonathan Ostinowsky <jonathan.ostinowsky@soteradefense.com>
2014-06-20 13:43:22 -0700

    Added DGARunnerExitTests and DGARunnerTests

--- dga-giraph/src/test/java/com/soteradefense/dga/DGAXMLConfigurationParserTest.java
+++ dga-giraph/src/test/java/com/soteradefense/dga/DGAXMLConfigurationParserTest.java
@@ -244,4 +244,56 @@ public class DGAXMLConfigurationParserTest {
         assertEquals("600000", conf.getCustomArgumentProperties().get("mapred.task.timeout"));
     }
 
+    @Test
+    public void testAllGiraphPropertiesThatCanBePresent() throws Exception{
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "\n" +
+                "<configuration>\n" +
+                "    <system>\n" +
+                "        <property>\n" +
+                "            <name>giraph.zkList</name>\n" +
+                "            <value>localhost:2181</value>\n" +
+                "        </property>\n" +
+                "    </system>\n" +
+                "    <custom>\n" +
+                "        <property>\n" +
+                "            <name>mapred.task.timeout</name>\n" +
+                "            <value>600000</value>\n" +
+                "        </property>\n" +
+                "    </custom>\n" +
+                "    <giraph>\n" +
+                "        <property>\n" +
+                "            <name>w</name>\n" +
+                "            <value>21</value>\n" +
+                "        </property>\n" +
+                "        <property>\n" +
+                "            <name>q</name>\n" +
+                "            <value></value>\n" +
+                "        </property>\n" +
+                "        <property>\n" +
+                "            <name>yj</name>\n" +
+                "            <value>/path/to/yarn/things</value>\n" +
+                "        </property>\n" +
+                "        <property>\n" +
+                "            <name>yh</name>\n" +
+                "            <value>1600</value>\n" +
+                "        </property>\n" +
+                "    </giraph>\n" +
+                "</configuration>";
+        byte [] xmlBytes = xml.getBytes();
+        InputStream bais = new ByteArrayInputStream(xmlBytes);
+
+        DGAConfiguration conf = DGAXMLConfigurationParser.parse(bais);
+        assertEquals("localhost:2181", conf.getSystemProperties().get("giraph.zkList"));
+
+        assertEquals(4, conf.getGiraphProperties().size());
+        assertEquals(1, conf.getCustomArgumentProperties().size());
+
+        assertEquals("21", conf.getGiraphProperties().get("-w"));
+        assertEquals("1600", conf.getGiraphProperties().get("-yh"));
+        assertEquals("/path/to/yarn/things", conf.getGiraphProperties().get("-yj"));
+        assertEquals("", conf.getGiraphProperties().get("-q"));
+        assertEquals("600000", conf.getCustomArgumentProperties().get("mapred.task.timeout"));
+    }
+
 }
