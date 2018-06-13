2587f27b6f0b509997f48a54403243f523a89d9b
George Zheng <george.zheng@soteradefense.com>
2018-06-13 12:59:43 -0400

    added new example for dga-yarn-graphx

--- dga-graphx/src/main/resources/run.sh
+++ dga-graphx/src/main/resources/run.sh
@@ -2,8 +2,14 @@
 
 # ./dga-mr1-graphx louvain -i tmp/dga/louvain/input/example.csv -o tmp/dga/louvain/output/ -s /opt/spark -n NameOfJob -m spark://localhost.localdomain:7077 --S spark.executor.memory=1g --ca parallelism=2 --S spark.worker.timeout=400 --S spark.cores.max=12
 
-./dga-mr1-graphx louvain -i tmp/dga/louvain/input/example.csv -o tmp/dga/louvain/output/ -s /opt/spark -n ExampleData -m spark://localhost.localdomain:7077 --S spark.executor.memory=1g --ca parallelism=2 --S spark.worker.timeout=400 --S spark.cores.max=12
+# ./dga-mr1-graphx louvain -i tmp/dga/louvain/input/example.csv -o tmp/dga/louvain/output/ -s /opt/spark -n ExampleData -m spark://localhost.localdomain:7077 --S spark.executor.memory=1g --ca parallelism=2 --S spark.worker.timeout=400 --S spark.cores.max=12
 
 # ./dga-mr1-graphx louvain -i tmp/dga/louvain/input/jeb.tsv -o tmp/dga/louvain/output/ -s /opt/spark -n JebBushData -m spark://localhost.localdomain:7077 --S spark.executor.memory=1g --ca parallelism=2 --S spark.worker.timeout=400 --S spark.cores.max=12
 
 # ./dga-mr1-graphx louvain -i tmp/dga/louvain/input/jeb.csv -o tmp/dga/louvain/output/ -s /opt/spark -n JebBushData -m spark://localhost.localdomain:7077 --S spark.executor.memory=1g --ca parallelism=2 --S spark.worker.timeout=400 --S spark.cores.max=12
+
+# assume the input folder contains a tsv file (src, dst, weight)
+#./dga-yarn-graphx louvain -i /user/gzheng/graphx/input -d '\t' -o /user/gzheng/graphx/output
+
+# assume the input folder corresponds to table with '\x01' (i.e., ^A) as delimiter 
+./dga-yarn-graphx louvain -i /user/hive/warehouse/gzheng.db/graphx_input -d '\x01' -o /tmp/trackcomms/graphx/output
